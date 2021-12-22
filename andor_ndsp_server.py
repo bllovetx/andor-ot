6748533f2c365b519b12886f084cfeb5d0bc9e05
曾许曌秋 <xzqzeng@gmail.com>
2021-12-22 21:14:00 +0800

    add comment for server

--- andor_ndsp_server.py
+++ andor_ndsp_server.py
@@ -3,11 +3,14 @@ from sipyco.pc_rpc import simple_server_loop
 import logging
 
 def main():
+    # config log
     logging.basicConfig(
         level=logging.INFO, filename="./logs/serverlog",
         format='%(asctime)s.%(msec)03d %(levelname)s %(module)s - %(funcName)s: %(message)s',
         datefmt='%Y-%m-%d %H:%M:%S'
     )
+    # start server
+    # TODO: change ip to current server's ip, NOTE don't use ::1(localhost)
     simple_server_loop({"emccd": ad.by_serial_number()}, "192.168.50.187", 3249)
 
 
