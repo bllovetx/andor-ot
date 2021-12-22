2c9bcfd45c939d22cc1e95764b1bb6a90d131b22
曾许曌秋 <xzqzeng@gmail.com>
2021-12-22 21:11:05 +0800

    add ndsp server

---
+++ andor_ndsp_server.py
@@ -0,0 +1,15 @@
+import andor as ad
+from sipyco.pc_rpc import simple_server_loop
+import logging
+
+def main():
+    logging.basicConfig(
+        level=logging.INFO, filename="./logs/mainlog",
+        format='%(asctime)s.%(msec)03d %(levelname)s %(module)s - %(funcName)s: %(message)s',
+        datefmt='%Y-%m-%d %H:%M:%S'
+    )
+    simple_server_loop({"emccd": ad.by_serial_number()}, "192.168.50.187", 3249)
+
+
+if __name__ == "__main__":
+    main()
